921ec654a5a9b58c13b4933eaf09ef8bf6c4dc81
7p3m1k <kor0885@gmail.com>
2021-04-02 16:23:02 +0900

    Initial Commit

---
+++ SwiftUIMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 박민규 on 2021/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
